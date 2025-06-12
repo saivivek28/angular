def5a7d7c618c3324a328d60f70dbf99125e6128
vivek <saiviveknakirikanti@gmail.com>
2025-06-12 12:38:10 +0530

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+ 
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,FormsModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  van=false;
+  name:string = "Angular App";
+  x: number = 10;
+  y: number = 20;
+  username = 'll';
+}
